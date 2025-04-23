556cd58f7b2270863beefd1474899eb015705057
Nikitos <gdhjyt68vhbe85.h@gmail.com>
2025-04-23 23:27:15 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,5 +4,5 @@ let text = document.getElementById('nikitos');
 button.addEventListener('click', () => {
     background.style.backgroundColor= '#daa0a0';
     alert('Avion style');
-    text.innerHTML = '<img src= "img/сюда.jpg" width = "400px"><h1>Вергиль сидит на стуле от Avion</h1>';
+    text.innerHTML = '<img src= "img/я с крыльями.jpg" width = "400px"><h1>сюда</h1>';
 });
